0a72a7c0b21d96297ef58a7910782a69a2a18ef7
Gary Willoughby <dev@nomad.so>
2018-11-06 21:03:18 +0000

    Reordered code and added token enum.

--- source/bfd/main.d
+++ source/bfd/main.d
@@ -8,6 +8,80 @@ import std.conv;
 import std.stdio;
 
 /**
+ * Brainfuck operators.
+ */
+enum bf
+{
+	right  = '>',
+	left   = '<',
+	add    = '+',
+	sub    = '-',
+	input  = ',',
+	output = '.',
+	open   = '[',
+	close  = ']',
+}
+
+/**
+ * Read the operators from the program and count the number of times they are
+ * used consecutively.
+ *
+ * Params:
+ *     program = The program to parse.
+ */
+auto parseOperators(string program)
+{
+	static struct Operator
+	{
+		char token;
+		int count;
+	}
+
+	Operator[] ops;
+	Operator current;
+
+	for (int x = 0; x < program.length; x++)
+	{
+		auto op = program[x];
+
+		switch(op)
+		{
+			case bf.right: goto case;
+			case bf.left: goto case;
+			case bf.add: goto case;
+			case bf.sub:
+				if (op == current.token)
+				{
+					current.count++;
+					break;
+				}
+
+				if (current.token != char.init)
+				{
+					ops ~= current;
+				}
+
+				current = Operator(op, 1);
+				break;
+
+			case bf.input: goto case;
+			case bf.output: goto case;
+			case bf.open: goto case;
+			case bf.close:
+				ops ~= current;
+				current = Operator(op, 1);
+				break;
+
+			default:
+				break;
+		}
+	}
+
+	ops ~= current;
+	return ops;
+}
+
+/**
  * Main entry point.
  *
  * Params:
@@ -36,31 +110,31 @@ void main(string[] args)
 	{
 		switch(ops[x].token)
 		{
-			case '>':
+			case bf.right:
 				cell += ops[x].count;
 				break;
-		
-			case '<':
+
+			case bf.left:
 				cell -= ops[x].count;
 				break;
-		
-			case '+':
+
+			case bf.add:
 				stack[cell] += ops[x].count;
 				break;
-		
-			case '-':
+
+			case bf.sub:
 				stack[cell] -= ops[x].count;
 				break;
-		
-			case '.':
-				printf("%c", stack[cell]);
-				break;
-		
-			case ',':
+
+			case bf.input:
 				stack[cell] = cast(char) getchar();
 				break;
-		
-			case '[':
+
+			case bf.output:
+				printf("%c", stack[cell]);
+				break;
+
+			case bf.open:
 				if (stack[cell] == '\0')
 				{
 					skip++;
@@ -83,8 +157,8 @@ void main(string[] args)
 					loops[++loop] = x;
 				}
 				break;
-		
-			case ']':
+
+			case bf.close:
 				if (stack[cell] == '\0')
 				{
 					loop--;
@@ -99,63 +173,4 @@ void main(string[] args)
 				break;
 		}
 	}
-}
-
-/**
- * Read the operators from the program and count the number of times they are
- * used consecutively.
- *
- * Params:
- *     program = The program to parse.
- */
-auto parseOperators(string program)
-{
-	static struct Operator
-	{
-		char token;
-		int count;
-	}
-
-	Operator[] ops;
-	Operator current;
-
-	for (int x = 0; x < program.length; x++)
-	{
-		auto op = program[x];
-
-		switch(op)
-		{
-			case '>': goto case;
-			case '<': goto case;
-			case '+': goto case;
-			case '-':
-					  if (op == current.token)
-					  {
-						  current.count++;
-						  break;
-					  }
-
-					  if (current.token != char.init)
-					  {
-						  ops ~= current;
-					  }
-
-					  current = Operator(op, 1);
-					  break;
-
-			case '.': goto case;
-			case ',': goto case;
-			case '[': goto case;
-			case ']':
-					  ops ~= current;
-					  current = Operator(op, 1);
-					  break;
-
-			default:
-					  break;
-		}
-	}
-
-	ops ~= current;
-	return ops;
 }
